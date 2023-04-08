f0639c86d4188f859f131aee891c4b2344bf4f23
Matt Ortner <matthewortner@gmail.com>
2023-04-08 13:10:15 -0400

    add socialIcons, flexWrap DevIcons

--- pages/components/DevIconGroup.js
+++ pages/components/DevIconGroup.js
@@ -2,47 +2,49 @@ import DevIcon from "./DevIcon";
 
 const DevIconGroup = () => {
   return (
-    <div className="grid grid-cols-2 gap-4">
-      <div className="grid gap-4">
-        <DevIcon
-          url={
-            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
-          }
-          alt={"Javascript Icon"}
-        />
-        <DevIcon
-          url={
-            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
-          }
-          alt={"Node Icon"}
-        />
-        <DevIcon
-          url={
-            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
-          }
-          alt={"Next.js Icon"}
-        />
-      </div>
-      <div className="grid gap-4">
-        <DevIcon
-          url={
-            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-          }
-          alt={"React Icon"}
-        />
-        <DevIcon
-          url={
-            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
-          }
-          alt={"MongoDB Icon"}
-        />
-        <DevIcon
-          url={
-            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
-          }
-          alt={"Tailwind CSS Icon"}
-        />
-      </div>
+    <div className="flex flex-wrap w-[500px] gap-4">
+      <DevIcon
+        url={
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+        }
+        alt={"Javascript Icon"}
+      />
+      <DevIcon
+        url={
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+        }
+        alt={"React Icon"}
+      />
+      <DevIcon
+        url={
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
+        }
+        alt={"Next.js Icon"}
+      />
+      <DevIcon
+        url={
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+        }
+        alt={"Node Icon"}
+      />
+      <DevIcon
+        url={
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
+        }
+        alt={"MongoDB Icon"}
+      />
+      <DevIcon
+        url={
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+        }
+        alt={"Tailwind CSS Icon"}
+      />
+      <DevIcon
+        url={
+          "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+        }
+        alt={"GIT Icon"}
+      />
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <NavBar />
       </div>
       <div className="fjac gap-10">
-        <div className="flex flex-col gap-16">
+        <div className="flex flex-col ml-28 gap-16">
           <div className="fjac">
             <Image
               src={profilePhoto}
@@ -37,18 +37,28 @@ export default function Home() {
               shadow-black shadow-lg w-[75%] rounded-2xl  hover:border-b-0 hover:mb-8
               hover:border-r-0 hover:rounded-none transition-all duration-100 ease-linear"
               >
-                {/* <div
-                className="bg-NAVY p-4 text-lg text-NAVY_T text-center 
-              border-b-8 border-r-8 border-double border-NAVY_T cursor-pointer
-              shadow-black shadow-lg w-[75%] rounded-2xl  hover:border-b-0 hover:mb-8
-              hover:border-r-0 hover:rounded-none transition-all duration-100 ease-linear"
-              > */}
                 Full-Stack Developer
               </div>
             </div>
           </div>
         </div>
         <DevIconGroup />
+      </div>
+      <div className="fjac mt-32 gap-7">
+        <SocialIcon
+          imageUrl={
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+          }
+          linkUrl={"https://github.com/MattOrtner"}
+          alt={"GitHub Icon and Link"}
+        />
+        <SocialIcon
+          imageUrl={
+            "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
+          }
+          linkUrl={"https://www.linkedin.com/in/matthewortner/"}
+          alt={"LinkedIn Icon and Link"}
+        />
       </div>
     </div>
   );
