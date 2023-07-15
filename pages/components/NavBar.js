c44f1f2b66b03e237d864b622c5eec8cb7932107
Matt Ortner <matthewortner@gmail.com>
2023-07-15 19:35:46 -0400

    attempt: with ?? for default value in href

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className="p-20 pb-24">
       <ul className="flex h-14 gap-10 justify-center text-center">
-        <NavButton href={{ auth: "/" }} buttonText={"Home"} />
+        <NavButton href={"/"} buttonText={"Home"} />
       </ul>
     </div>
   );
--- pages/components/buttons/NavButton.js
+++ pages/components/buttons/NavButton.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 const NavButton = ({ href, buttonText }) => {
   return (
     <Link
-      href={href}
+      href={href ?? ""}
       className="w-40 cursor-pointer bg-NAVY fjac text-NAVY_T text-lg
           border-b-8 border-r-8 border-NAVY_T rounded-trs-2xl border-double shadow-black shadow-lg
           hover:border-b-0 hover:border-r-0 hover:rounded-none transition-all duration-100 ease-linear"
