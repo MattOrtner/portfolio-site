5f7421c33a2c6eb2719d67d23af565f95b94b200
Matt Ortner <matthewortner@gmail.com>
2025-03-07 12:38:50 -0500

    change tab name

--- pages/components/HeaderSection.js
+++ pages/components/HeaderSection.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 const HeaderSection = () => {
   return (
     <Head>
-      <title>Welcome to the party!</title>
+      <title>Full-Stack & Beyond</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="software developer portfolio site" />
     </Head>
