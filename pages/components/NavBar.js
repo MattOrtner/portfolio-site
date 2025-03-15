889a09fb0c1e460add5f2242dc28bd3ffc1a75b6
Matt Ortner <matthewortner@gmail.com>
2025-03-15 09:47:59 -0400

    fix: center project page and align elements

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -2,8 +2,8 @@ import NavButton from "./buttons/NavButton";
 
 const NavBar = () => {
   return (
-    <div className="w-full flex justify-end p-6 pb-14">
-      <ul className="h-14">
+    <div className="w-full flex justify-end pr-10 max-[400px]:pr-2 max-[400px]:pt-2">
+      <ul>
         <NavButton href={"/"} buttonText={"Back"} />
       </ul>
     </div>
--- pages/projects.js
+++ pages/projects.js
@@ -5,53 +5,57 @@ import PortfolioPhoto from "../public/PortfolioPhoto.png";
 
 const Projects = () => {
   return (
-    <div className="p-4 w-screen h-screen font-mono flex items-center flex-col gap-2 max-w-[1400px] max-[400px]:h-full">
-      <NavBar />
-      <a
-        href="https://staging.d2yhpv0ems9pvt.amplifyapp.com"
-        className="flex gap-4 max-[400px]:grid max-[400px]:p-3 max-[400px]:gap-2"
-      >
-        <div className="bg-NAVY text-NAVY_T rounded-2xl flex gap-6 flex-col pt-10 pb-20 px-20 max-[400px]:px-5 max-[400px]:py-5 max-[400px]:order-first">
-          <h1 className="text-3xl">A Clone of Etsy</h1>
-          <ul className="flex justify-evenly flex-col h-[100%] text-lg">
-            <li>React</li>
-            <li>React Router</li>
-            <li>Styled Components</li>
-            <li>
-              AWS
-              <li>-Amplify</li>
-              <li>-Cloudformation</li>
-              <li>-DynamoDB</li>
-              <li>-AWS SAM</li>
-            </li>
-          </ul>
+    <div className="bg-gray-200 flex justify-center items-center">
+      <div className="m-4 flex-col gap-2 max-w-[1400px] max-[400px]:h-full">
+        <NavBar />
+        <div className="m-10 flex gap-4 max-[400px]:grid max-[400px]:m-3 max-[400px]:gap-2">
+          <div className="bg-NAVY text-NAVY_T rounded-2xl flex gap-6 flex-col pt-10 pb-20 px-20 max-[400px]:px-5 max-[400px]:py-5 max-[400px]:order-first">
+            <h1 className="text-3xl">A Clone of Etsy</h1>
+            <ul className="flex justify-evenly flex-col h-[100%] text-lg">
+              <li>React</li>
+              <li>React Router</li>
+              <li>Styled Components</li>
+              <li>
+                AWS
+                <li>-Amplify</li>
+                <li>-Cloudformation</li>
+                <li>-DynamoDB</li>
+                <li>-AWS SAM</li>
+              </li>
+            </ul>
+          </div>
+          <a
+            href="https://staging.d2yhpv0ems9pvt.amplifyapp.com"
+            // className="flex gap-4 max-[400px]:grid max-[400px]:m-3 max-[400px]:gap-2"
+          >
+            <Image
+              src={EtsyScreenShot}
+              alt="A screen shot of Matt's Etsy Clone Project"
+              className="shadow-black shadow-2xl rounded-3xl border-blue-950 border-4"
+              width={700}
+              height={"auto"}
+              priority
+            />
+          </a>
         </div>
-        <Image
-          src={EtsyScreenShot}
-          alt="A screen shot of Matt's Etsy Clone Project"
-          className="shadow-black shadow-2xl rounded-3xl border-blue-950 border-4"
-          width={700}
-          height={"auto"}
-          priority
-        />
-      </a>
-      <div className="p-10 flex gap-4 max-[400px]:grid max-[400px]:p-3 max-[400px]:gap-2">
-        <div className="bg-NAVY text-NAVY_T rounded-2xl flex gap-6 flex-col pt-10 pb-20 px-20 max-[400px]:px-5 max-[400px]:py-5 max-[400px]:order-first">
-          <h1 className="text-3xl">This Portfolio</h1>
-          <ul className="flex justify-evenly flex-col h-[100%] text-lg">
-            <li>Next.js</li>
-            <li>Tailwind CSS</li>
-            <li>Vercel</li>
-          </ul>
+        <div className="m-10 flex gap-4 max-[400px]:grid max-[400px]:m-3 max-[400px]:gap-2">
+          <div className="bg-NAVY text-NAVY_T rounded-2xl flex gap-6 flex-col pt-10 pb-20 px-20 max-[400px]:px-5 max-[400px]:py-5 max-[400px]:order-first">
+            <h1 className="text-3xl">This Portfolio</h1>
+            <ul className="flex justify-evenly flex-col h-[100%] text-lg">
+              <li>Next.js</li>
+              <li>Tailwind CSS</li>
+              <li>Vercel</li>
+            </ul>
+          </div>
+          <Image
+            src={PortfolioPhoto}
+            alt="A screen shot of his Etsy Clone Project"
+            className="shadow-black shadow-2xl rounded-3xl border-blue-950 border-4"
+            width={700}
+            height={"auto"}
+            priority
+          />
         </div>
-        <Image
-          src={PortfolioPhoto}
-          alt="A screen shot of his Etsy Clone Project"
-          className="shadow-black shadow-2xl rounded-3xl border-blue-950 border-4"
-          width={700}
-          height={"auto"}
-          priority
-        />
       </div>
     </div>
   );
